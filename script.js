0ad28f6dbc5ed8bc72efc1906c0caaccf4a84189
ousama al saedi <ousama269@icloud.com>
2023-08-31 14:09:55 +0000

    first function, it flips card and looks for a match

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const cards = document.querySelectorAll("#card")
 let hasFlippedCard = false;
 let lockTable = false;
 let firstCard, secondCard;
+
+function flipCard() {
+    if (lockTable) return ;
+    if (this === firstCard) return;
+
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        hasFlippedCard =true;
+        firstCard = this;
+        return;
+    }
+    secondCard=this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+    isMatch?disableCards(): unflipCards();
+}
